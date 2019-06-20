56e30b462172d024130f424aeb2bf4757ecf09bb
sainis <sanjeev.saini@dowjones.com>
2019-06-20 12:03:22 -0400

    Checking in first python programming file

---
+++ condition.py
@@ -0,0 +1,8 @@
+item =  input("Enter the qty you want to buy: ")
+
+if item > 5:
+    print("You get 10% discount")
+elif item >=3 and item <=5:
+    print("Thanks")
+elif item < 3:
+    print("Buy more")
