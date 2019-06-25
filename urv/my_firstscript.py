0f6785509d73912825e2617d8573831db03b7af0
unayak <urvashi.nayak@bedbath.com>
2019-06-24 22:20:06 -0400

    my first program

---
+++ urv/my_firstscript.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+num1 = 5
+num2 = 6
+sum = num1 + num2 
+diff = num1 - num2 
+mul = num1 * num2 
+div = num1 / num2 
+
+# sum 
+print("The of the two numnber is :" + str(sum))
+
+#diff
+print("\nThe diff of two numnber is :" + str(diff))
+
+#mul
+print("\nThe multiplicaiton of two number is :" + str(mul))
+
+#division
+print("\nThe Division of two numbers is :" + str(div))
+
